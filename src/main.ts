e33f676e9aadfcefccbe6a774e9c05afbd18343c
Jimly-Firdaus <13521102@std.stei.itb.ac.id>
2024-05-12 16:05:42 +0700

    chore: clean up

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,13 @@
 // Stylesheet imports
 import "src/css/global.css";
 import { setupLoadModel } from "src/ui-element/load-model.ts";
-// import "src/css/dummy/dummy.css";
 
 /**
  * Main Script
 */
-const loadFile = document.querySelector<HTMLInputElement>("#load-model")!;
-setupLoadModel(loadFile);
+const main = () => {
+  const loadFile = document.querySelector<HTMLInputElement>("#load-model")!;
+  setupLoadModel(loadFile);
+}
+
+main()
