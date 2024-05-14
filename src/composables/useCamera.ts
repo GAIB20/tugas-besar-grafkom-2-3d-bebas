9fd3bfbed94aa304292e7182188678835e9d353d
Jimly-Firdaus <13521102@std.stei.itb.ac.id>
2024-05-14 22:22:05 +0700

    feat(camera): useCamera composables

---
+++ src/composables/useCamera.ts
@@ -0,0 +1,60 @@
+import { ObliqueCamera } from "src/cameras/oblique";
+import { OrthographicCamera } from "src/cameras/orthographic";
+import { PerspectiveCamera } from "src/cameras/perspective";
+import { WebGLRenderer } from "src/webgl/renderer";
+
+export const useCamera = (renderer: WebGLRenderer) => {
+  /**
+   * Orthographic Camera
+   * */
+  const ORTHOGRAPHIC_NEAR = -1000;
+  const ORTHOGRAPHIC_FAR = 1000;
+  const orthographicCam = new OrthographicCamera(
+    -renderer.width / 2,
+    renderer.width / 2,
+    renderer.height / 2,
+    -renderer.height / 2,
+    ORTHOGRAPHIC_NEAR,
+    ORTHOGRAPHIC_FAR
+  );
+
+  /**
+   * Perspective Camera
+   * */
+  const PERSPECTIVE_FOV = 60;
+  const PERSPECTIVE_NEAR = 0.01;
+  const PERSPECTIVE_FAR = 1000;
+  const perspectiveCam = new PerspectiveCamera(
+    PERSPECTIVE_FOV,
+    renderer.width / renderer.height,
+    PERSPECTIVE_NEAR,
+    PERSPECTIVE_FAR
+  );
+  perspectiveCam.position.z = 700;
+
+  /**
+   * Oblique Camera
+   * */
+  const OBLIQUE_THETA = 45;
+  const OBLIQUE_PHI = 45;
+  const obliqueCam = new ObliqueCamera(
+    OBLIQUE_THETA,
+    OBLIQUE_PHI,
+    -renderer.width / 2,
+    renderer.width / 2,
+    renderer.height / 2,
+    -renderer.height / 2,
+    ORTHOGRAPHIC_NEAR,
+    ORTHOGRAPHIC_FAR
+  );
+
+  const cameras = {
+    ORTHOGRAPHIC_CAM: orthographicCam,
+    PERSPECTIVE_CAM: perspectiveCam,
+    OBLIQUE_CAM: obliqueCam,
+  };
+
+  return {
+    cameras,
+  };
+};
