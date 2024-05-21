5200fa73eac441450c4063b5c6d3514d8a87a23c
Jimly-Firdaus <13521102@std.stei.itb.ac.id>
2024-05-22 00:42:21 +0700

    chore: add element bindings

---
+++ src/ui-element/bindings.ts
@@ -0,0 +1,106 @@
+/**
+ * Bindings to HTML Element
+ */
+
+const getElement = (id: string) => document.getElementById(id);
+
+// Load & Save
+const loadInput = getElement("load-model") as HTMLInputElement;
+const saveInput = getElement("save-model") as HTMLInputElement;
+
+// Camera type
+const camSelectInput = getElement("projection-type") as HTMLSelectElement;
+const resetCamBtn = getElement("reset-cam") as HTMLButtonElement;
+const camDistanceInput = getElement("cam-distance") as HTMLInputElement;
+
+// Texture
+const textureSelectInput = getElement("mapping-type") as HTMLSelectElement;
+
+// Translation
+const xTranslationInput = getElement("x-translation") as HTMLInputElement;
+const yTranslationInput = getElement("y-translation") as HTMLInputElement;
+const zTranslationInput = getElement("z-translation") as HTMLInputElement;
+
+// Rotation
+const xRotationInput = getElement("x-rotation") as HTMLInputElement;
+const yRotationInput = getElement("y-rotation") as HTMLInputElement;
+const zRotationInput = getElement("z-rotation") as HTMLInputElement;
+
+// Scale
+const xScaleInput = getElement("x-scale") as HTMLInputElement;
+const yScaleInput = getElement("y-scale") as HTMLInputElement;
+const zScaleInput = getElement("z-scale") as HTMLInputElement;
+
+// Animation
+const animTogglePlay = getElement("animation-toggle-play") as HTMLInputElement;
+const animToggleReverse = getElement(
+  "animation-toggle-reverse"
+) as HTMLInputElement;
+const animToggleAutoPlay = getElement(
+  "animation-toggle-auto-play"
+) as HTMLInputElement;
+const animationToggleFrameNav = getElement(
+  "animation-toggle-frame-nav"
+) as HTMLInputElement;
+const animationFrameData = getElement(
+  "animation-frame-data"
+) as HTMLParagraphElement;
+const animToggleNextFrame = getElement(
+  "animation-toggle-next-frame"
+) as HTMLButtonElement;
+const animTogglePrevFrame = getElement(
+  "animation-toggle-prev-frame"
+) as HTMLButtonElement;
+
+const animControllerInsertFrame = getElement(
+  "animation-controller-insert-frame"
+) as HTMLButtonElement;
+const animControllerDeleteFrame = getElement(
+  "animation-controller-delete-frame"
+) as HTMLButtonElement;
+const animControllerEditFrame = getElement(
+  "animation-controller-edit-frame"
+) as HTMLButtonElement;
+const animControllerSwapFrame = getElement(
+  "animation-controller-swap-frame"
+) as HTMLButtonElement;
+
+export {
+  // Load n save
+  loadInput,
+  saveInput,
+
+  // Cam
+  camSelectInput,
+  resetCamBtn,
+  camDistanceInput,
+
+  // Texture
+  textureSelectInput,
+
+  // Transformation
+  xTranslationInput,
+  yTranslationInput,
+  zTranslationInput,
+  xRotationInput,
+  yRotationInput,
+  zRotationInput,
+  xScaleInput,
+  yScaleInput,
+  zScaleInput,
+
+  // Animation
+  animTogglePlay,
+  animToggleReverse,
+  animToggleAutoPlay,
+  animationToggleFrameNav,
+  animationFrameData,
+  animToggleNextFrame,
+  animTogglePrevFrame,
+
+  // Animation Controller
+  animControllerInsertFrame,
+  animControllerDeleteFrame,
+  animControllerEditFrame,
+  animControllerSwapFrame,
+};
